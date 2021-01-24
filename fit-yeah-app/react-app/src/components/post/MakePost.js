0200575a0bd3c2d72f25cb3b772e2a907b9c111a
ryoung7986 <ryoung7986@gmail.com>
2021-01-24 16:25:12 -0500

    added notification when media is uploaded to a post

--- fit-yeah-app/react-app/src/components/post/MakePost.js
+++ fit-yeah-app/react-app/src/components/post/MakePost.js
@@ -7,7 +7,7 @@ import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import UploadImgForm from '../UploadImgForm';
 import UploadVideoForm from '../UploadVideoForm';
 import Modal from 'react-modal';
-import ReactPlayer from 'react-player';
+import PermMediaIcon from '@material-ui/icons/PermMedia';
 import './MakePost.css';
 
 function MakePost({ user }) {
@@ -64,98 +64,103 @@ function MakePost({ user }) {
   }
 
   return (
-    <div className='makePost'>
-      <div className="makePost__top">
-        <Avatar src={user.avatar_url} />
-        <form ref={form} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="makePost__input"
-            placeholder={`What's on your mind, ${user.first_name}?`} />
-          <button
-            type="submit">
-            Hidden Submit
-          </button>
-        </form>
-        <div className="makePost__imgAdded">
-          {modalRef.current ? <p>Media added</p> : null}
+    <>
+      <div className='makePost'>
+        <div className="makePost__top">
+          <Avatar src={user.avatar_url} />
+          <form ref={form} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="makePost__input"
+              placeholder={`What's on your mind, ${user.first_name}?`} />
+            <button
+              type="submit">
+              Hidden Submit
+            </button>
+          </form>
+        </div>
+        <div className="makePost__bottom">
+          <div className="makePost__option" onClick={() => setVideoUploadDisplay(true)}>
+            <VideocamIcon />
+            <h3>Add Video</h3>
+          </div>
+          <div className="makePost__option" onClick={() => setImgUploadDisplay(true)}>
+            <AddPhotoAlternateIcon />
+            <h3>Add Photo</h3>
+          </div>
+          {imgUploadDisplay && (
+            <Modal
+              isOpen={imgUploadDisplay}
+              onRequestClose={onModalClose}
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(16, 125, 126, 0.5)'
+                },
+                content: {
+                  position: 'absolute',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'space-evenly',
+                  justifyContent: 'space-evenly',
+                  top: '33%',
+                  left: '33%',
+                  right: '33%',
+                  bottom: '33%',
+                  border: '1px solid #ccc',
+                  background: '#107D7E',
+                  borderRadius: '8px',
+                  padding: '20px',
+                  boxShadow: "0px 5px 8px -2px rgba(0, 0, 0, 0.75);"
+                }
+              }}
+            >
+              <div className="modal__body">
+                <UploadImgForm ref={modalRef} />
+              </div>
+            </Modal>
+          )}
+          {videoUploadDisplay && (
+            <Modal
+              isOpen={videoUploadDisplay}
+              onRequestClose={onVidModalClose}
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(16, 125, 126, 0.5)'
+                },
+                content: {
+                  position: 'absolute',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'space-evenly',
+                  justifyContent: 'space-evenly',
+                  top: '33%',
+                  left: '33%',
+                  right: '33%',
+                  bottom: '33%',
+                  border: '1px solid #ccc',
+                  background: '#107D7E',
+                  borderRadius: '8px',
+                  padding: '20px',
+                  boxShadow: "0px 5px 8px -2px rgba(0, 0, 0, 0.75);"
+                }
+              }}
+            >
+              <div className="modal__body">
+                <UploadVideoForm ref={modalRef} />
+              </div>
+            </Modal>
+          )}
         </div>
       </div>
-      <div className="makePost__bottom">
-        <div className="makePost__option" onClick={() => setVideoUploadDisplay(true)}>
-          <VideocamIcon />
-          <h3>Add Video</h3>
-        </div>
-        <div className="makePost__option" onClick={() => setImgUploadDisplay(true)}>
-          <AddPhotoAlternateIcon />
-          <h3>Add Photo</h3>
-        </div>
-        {imgUploadDisplay && (
-          <Modal
-            isOpen={imgUploadDisplay}
-            onRequestClose={onModalClose}
-            style={{
-              overlay: {
-                backgroundColor: 'rgba(16, 125, 126, 0.5)'
-              },
-              content: {
-                position: 'absolute',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'space-evenly',
-                justifyContent: 'space-evenly',
-                top: '33%',
-                left: '33%',
-                right: '33%',
-                bottom: '33%',
-                border: '1px solid #ccc',
-                background: '#107D7E',
-                borderRadius: '8px',
-                padding: '20px',
-                boxShadow: "0px 5px 8px -2px rgba(0, 0, 0, 0.75);"
-              }
-            }}
-          >
-            <div className="modal__body">
-              <UploadImgForm ref={modalRef} />
-            </div>
-          </Modal>
-        )}
-        {videoUploadDisplay && (
-          <Modal
-            isOpen={videoUploadDisplay}
-            onRequestClose={onVidModalClose}
-            style={{
-              overlay: {
-                backgroundColor: 'rgba(16, 125, 126, 0.5)'
-              },
-              content: {
-                position: 'absolute',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'space-evenly',
-                justifyContent: 'space-evenly',
-                top: '33%',
-                left: '33%',
-                right: '33%',
-                bottom: '33%',
-                border: '1px solid #ccc',
-                background: '#107D7E',
-                borderRadius: '8px',
-                padding: '20px',
-                boxShadow: "0px 5px 8px -2px rgba(0, 0, 0, 0.75);"
-              }
-            }}
-          >
-            <div className="modal__body">
-              <UploadVideoForm ref={modalRef} />
-            </div>
-          </Modal>
-        )}
-      </div>
-    </div>
+      {modalRef.current ?
+        <div className="makePost__mediaAdded">
+          <PermMediaIcon />
+          <p>Media added</p>
+        </div> : null}
+
+    </>
   )
 }
 
