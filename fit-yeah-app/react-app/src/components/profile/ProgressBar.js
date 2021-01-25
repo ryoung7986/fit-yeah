8efa53139fc8fe1876268d657e6d999362a5e9e6
ryoung7986 <ryoung7986@gmail.com>
2021-01-24 22:09:55 -0500

    fixed progress bar percentage

--- fit-yeah-app/react-app/src/components/profile/ProgressBar.js
+++ fit-yeah-app/react-app/src/components/profile/ProgressBar.js
@@ -10,40 +10,40 @@ function ProgressBar() {
 
 
   if (userPoints < 1000) {
-    done = (userPoints / 100) * 100
-  }
-  if (userPoints >= 1000 && userPoints < 3000) {
-    done = (userPoints / 500) * 100
-  }
-  if (userPoints >= 3000 && userPoints < 6000) {
     done = (userPoints / 1000) * 100
   }
-  if (userPoints >= 6000 && userPoints < 10000) {
-    done = (userPoints / 1500) * 100
-  }
-  if (userPoints >= 10000 && userPoints < 15000) {
-    done = (userPoints / 2000) * 100
-  }
-  if (userPoints >= 15000 && userPoints < 21000) {
-    done = (userPoints / 2500) * 100
-  }
-  if (userPoints >= 21000 && userPoints < 28000) {
+  if (userPoints >= 1000 && userPoints < 3000) {
     done = (userPoints / 3000) * 100
   }
+  if (userPoints >= 3000 && userPoints < 6000) {
+    done = (userPoints / 6000) * 100
+  }
+  if (userPoints >= 6000 && userPoints < 10000) {
+    done = (userPoints / 10000) * 100
+  }
+  if (userPoints >= 10000 && userPoints < 15000) {
+    done = (userPoints / 15000) * 100
+  }
+  if (userPoints >= 15000 && userPoints < 21000) {
+    done = (userPoints / 21000) * 100
+  }
+  if (userPoints >= 21000 && userPoints < 28000) {
+    done = (userPoints / 28000) * 100
+  }
   if (userPoints >= 28000 && userPoints < 36000) {
-    done = (userPoints / 3500) * 100
+    done = (userPoints / 36000) * 100
   }
   if (userPoints >= 36000 && userPoints < 45000) {
-    done = (userPoints / 4000) * 100
+    done = (userPoints / 45000) * 100
   }
   if (userPoints >= 45000 && userPoints < 55000) {
-    done = (userPoints / 4500) * 100
+    done = (userPoints / 55000) * 100
   }
   if (userPoints >= 55000 && userPoints < 66000) {
-    done = (userPoints / 5000) * 100
+    done = (userPoints / 66000) * 100
   }
   if (userPoints >= 66000 && userPoints < 78000) {
-    done = (userPoints / 5500) * 100
+    done = (userPoints / 78000) * 100
   }
   if (userPoints >= 78000) {
     done = 100
@@ -59,7 +59,7 @@ function ProgressBar() {
           width: `${done}%`
         }}
       >
-        <p>{Math.floor(done)}%</p>
+        {done < 15 ? null : <p>{Math.floor(done)}%</p>}
       </div>
     </div>
   )
