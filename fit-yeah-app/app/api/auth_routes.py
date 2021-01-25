52a4e653087189c548fea58c0e21c5ac227c6d08
ryoung7986 <ryoung7986@gmail.com>
2021-01-24 21:44:54 -0500

    included points_earned in user signup

--- fit-yeah-app/app/api/auth_routes.py
+++ fit-yeah-app/app/api/auth_routes.py
@@ -66,7 +66,8 @@ def sign_up():
             first_name=form.data['first_name'],
             last_name=form.data['last_name'],
             email=form.data['email'],
-            password=form.data['password']
+            password=form.data['password'],
+            points_earned=0
         )
         db.session.add(user)
         db.session.commit()
