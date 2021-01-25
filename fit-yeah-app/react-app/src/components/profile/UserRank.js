225b8d97c53c588209816acd0f5150f55e87eb63
ryoung7986 <ryoung7986@gmail.com>
2021-01-24 21:56:32 -0500

    updated progression points for ranks

--- fit-yeah-app/react-app/src/components/profile/UserRank.js
+++ fit-yeah-app/react-app/src/components/profile/UserRank.js
@@ -5,67 +5,67 @@ function UserRank({ user }) {
   const points = user.points_earned;
 
   const userRank = () => {
-    if (points < 100) {
+    if (points < 1000) {
       return (
         <p>Rank: Novice</p>
       )
     }
-    if (points >= 100 && points < 500) {
+    if (points >= 1000 && points < 3000) {
       return (
         <p>Rank: Rookie</p>
       )
     }
-    if (points >= 500 && points < 1000) {
+    if (points >= 3000 && points < 6000) {
       return (
         <p>Rank: Flyweight</p>
       )
     }
-    if (points >= 1000 && points < 1500) {
+    if (points >= 6000 && points < 10000) {
       return (
         <p>Rank: Bantamweight</p>
       )
     }
-    if (points >= 1500 && points < 2000) {
+    if (points >= 10000 && points < 15000) {
       return (
         <p>Rank: Featherweight</p>
       )
     }
-    if (points >= 2000 && points < 2500) {
+    if (points >= 15000 && points < 21000) {
       return (
         <p>Rank: Lightweight</p>
       )
     }
-    if (points >= 2500 && points < 3000) {
+    if (points >= 21000 && points < 28000) {
       return (
         <p>Rank: Welterweight</p>
       )
     }
-    if (points >= 3000 && points < 3500) {
+    if (points >= 28000 && points < 36000) {
       return (
         <p>Rank: Middleweight</p>
       )
     }
-    if (points >= 3500 && points < 4000) {
+    if (points >= 36000 && points < 45000) {
       return (
         <p>Rank: Light Heavyweight</p>
       )
     }
-    if (points >= 4000 && points < 4500) {
+    if (points >= 45000 && points < 55000) {
       return (
         <p>Rank: Heavyweight</p>
       )
     }
-    if (points >= 4500 && points < 5000) {
+    if (points >= 55000 && points < 66000) {
       return (
         <p>Rank: Super Heavyweight</p>
       )
     }
-    if (points >= 5000 && points < 5500) {
+    if (points >= 66000 && points < 78000) {
       return (
         <p>Rank: Contender</p>
       )
     }
-    if (points >= 5500) {
+    if (points >= 78000) {
       return (
         <p>Rank: Champ</p>
       )
